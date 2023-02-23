a273170f4765c725225d86ef8df20ab68ce8ea7b
John Waczak <john.louis.waczak@gmail.com>
2023-02-23 09:30:49 -0600

    updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using MasterChemicalMechanism
 using Test
 
 @testset "atmospheric_functions.jl" begin
+    # check at standard temperature and pressure
     M_stp = MasterChemicalMechanism.calcAirDensity(1013.2, 273.15)
     @test isapprox(M_stp, 2.6866e19, rtol=1e-4)
 end
